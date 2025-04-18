7bcce872d3b67f07205d5a1530996a7e720e04a4
TedW-g <tedw3@nycstudents.net>
2025-04-18 01:42:12 +0000

    commit

--- Data.java
+++ Data.java
@@ -3,30 +3,56 @@ public class Data
     public static final int MAX = (int) (Math.random() * Integer.MAX_VALUE);
     private int[][] grid;
 
+    public Data(int[][] arr)
+    {
+        grid = arr;
+    }
+
     public Data(int rows, int cols)
     {
         grid = new int[rows][cols];
     }
 
-    public int[][] getGrid()
-    {
-        return grid;
-    }
-
     public void repopulate()
     {
-        for (int i = 0; i < grid.length; i++)
+        for (int row = 0; row < grid.length; row++)
         {
-            for (int o = 0; o < grid[0].length; o++)
+            for (int col = 0; col < grid[0].length; col++)
             {
-                grid[i][o] = MAX;
+                int random = 0;
+                while(random % 10 != 0 || random % 100 == 0) random = (int) (Math.random() * MAX + 1);
+                grid[row][col] = random;
             }
         }
     }
 
     public int countIncreasingCols() 
     {
-        return 0;
+        int count = 0;
+        for (int col = 0; col < grid[0].length; col++)
+        {
+            boolean ascend = true;
+            for (int row = 1; row < grid.length; row++)
+            {
+                if (grid[row-1][col] > grid[row][col])
+                { 
+                    ascend = false;
+                }
+            }
+            if(ascend) count++;
+        }
+        return count;
+    }
+
+    public String toString()
+    {
+        String s = "";
+        for (int[] row : grid)
+        {
+            for (int col : row) s += col + " ";
+            s += "\n";
+        }
+        return s;
     }
 }
 
--- Main.java
+++ Main.java
@@ -4,6 +4,28 @@ public class Main
     {
         System.out.println(Data.MAX);
         Data dat = new Data(3,3);
-        System.out.println(dat.getGrid());
+        System.out.println(dat);
+        dat.repopulate();
+        System.out.println(dat);
+        
+        int[][] grid1 =
+        {
+            {10,50,40},
+            {20,40,20},
+            {30,50,30}
+        };
+        dat = new Data(grid1);
+        System.out.println(dat);
+        System.out.println(dat.countIncreasingCols());
+        System.out.println();
+
+        int[][] grid2 = 
+        {
+            {10,540,440,440},
+            {220,450,440,190}
+        };
+        dat = new Data(grid2);
+        System.out.println(dat);
+        System.out.println(dat.countIncreasingCols());
     }
 }
